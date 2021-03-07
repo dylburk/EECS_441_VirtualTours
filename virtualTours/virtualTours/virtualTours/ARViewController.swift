08bdaa00f3a2901ccef7ac8d6b20f2a230db926c
Hunter Harloff <harloff@umich.edu>
2021-03-07 18:11:45 -0500

    Render cube

--- virtualTours/virtualTours/virtualTours/ARViewController.swift
+++ virtualTours/virtualTours/virtualTours/ARViewController.swift
@@ -23,8 +23,8 @@ class ARViewController: UIViewController {
         
         arView.session.run(configuration)
         
-        //let cubeNode = SCNNode(geometry: SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius: 0))
-        //cubeNode.position = SCNVector3(0, 0, -0.2) // SceneKit/AR coordinates are in meters
-        //arView.scene.rootNode.addChildNode(cubeNode)
+        let cubeNode = SCNNode(geometry: SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius: 0))
+        cubeNode.position = SCNVector3(0, 0, -0.2) // SceneKit/AR coordinates are in meters
+        arView.scene.rootNode.addChildNode(cubeNode)
     }
 }
