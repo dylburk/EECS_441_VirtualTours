74de16b4b531ee768e3733fd840d29fb97e139fe
Nicholas Keller <nkeller97@gmail.com>
2021-04-18 18:20:57 -0400

    Cleaned up NearbyStore.swift

--- virtualTours/virtualTours/virtualTours/NearbyStore.swift
+++ virtualTours/virtualTours/virtualTours/NearbyStore.swift
@@ -13,13 +13,11 @@ struct NearbyStore {
         let long = String(format: "%.6f", currentLocation.coordinate.longitude)
         var modifiedUrl = serverUrl
         let defaults = UserDefaults.standard
-        
         let sendNotifs = defaults.bool(forKey: sendNotifKey)
         if (!sendNotifs) {
             print("user disabled notifications")
             return
         }
-        
         if let phoneNumberValue = defaults.string(forKey: phoneNumberKey) {
             modifiedUrl = serverUrl + "?phone=" + phoneNumberValue
             modifiedUrl = modifiedUrl + "&lat=" + lat + "&long=" + long
@@ -27,17 +25,12 @@ struct NearbyStore {
         } else {
             return
         }
-        
-        
-        
         guard let apiUrl = URL(string: modifiedUrl) else {
             print("nearbySMS: Bad URL")
             return
         }
-        
         var request = URLRequest(url: apiUrl)
         request.httpMethod = "GET"
-        
         let task = URLSession.shared.dataTask(with: request) { data, response, error in
             defer { completion() }
             if let error = error {
@@ -46,16 +39,12 @@ struct NearbyStore {
                 if httpResponse.statusCode == 200 {
                     print(data!)
                     return
-                }
-                else{
+                } else {
                     print("nearbySMS: HTTP STATUS: \(httpResponse.statusCode)")
                     return
                 }
             }
-            
-            
         }
         task.resume()
     }
-
 }
